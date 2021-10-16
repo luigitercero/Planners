9b67af14134ecc380213b2ee88fc05dab1ac3e49
luigitercerobold <luigitercero@gmail.com>
2021-10-15 19:54:13 -0600

    20 *20 matriz dde caminos

--- caminos.py
+++ caminos.py
@@ -1,10 +1,16 @@
-for i in range(1, 16):
-  if(i % 4) != 0:
+for i in range(1, 400):
+  if(i % 20) != 0:
     print("(goto l"+ str(i) +" l" + str(i+1)+")")
     print("(goto l"+ str(i+1) +" l" + str(i)+")")
 
 print (";horizontal")
-for i in range(1, 16):
-  if i+4 < 17 :
+for i in range(1, 400):
+  if i+4 <= 400 :
     print("(goto l"+ str(i) +" l" + str(i+4)+")")
     print("(goto l"+ str(i+4) +" l" + str(i)+")")
+
+print (";variables")
+a =""
+for i in range(1, 400):
+  a = "l"+str(i) +" "+a
+#print(a)
