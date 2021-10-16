c17fc8fd46342d1b9bdf0411a1564f1b1e6ac536
luigitercerobold <luigitercero@gmail.com>
2021-10-16 10:40:32 -0600

    ultimo_problema

--- caminos.py
+++ caminos.py
@@ -1,16 +1,17 @@
-for i in range(1, 400):
-  if(i % 20) != 0:
+exp = 7
+for i in range(1, exp*exp):
+  if(i % exp) != 0:
     print("(goto l"+ str(i) +" l" + str(i+1)+")")
     print("(goto l"+ str(i+1) +" l" + str(i)+")")
 
 print (";horizontal")
-for i in range(1, 400):
-  if i+4 <= 400 :
+for i in range(1, exp*exp):
+  if i+4 <= exp*exp :
     print("(goto l"+ str(i) +" l" + str(i+4)+")")
     print("(goto l"+ str(i+4) +" l" + str(i)+")")
 
 print (";variables")
 a =""
-for i in range(1, 400):
+for i in range(1, exp*exp):
   a = "l"+str(i) +" "+a
-#print(a)
+print(a)
